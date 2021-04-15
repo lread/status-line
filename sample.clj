907f3e64dd94f07bf868ee4e62e75936f7b898fa
lread <lee@dlread.com>
2021-04-15 15:48:36 -0400

    Add some initial code and docs

---
+++ sample.clj
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+(require '[lread.status-line :as status])
+
+(status/line :head "Sample heading")
+(status/line :warn "Sample warning")
+(status/line :error "Sample errror")
+
+(status/line :head "Multiple line\nheading sample\nhere")
+(status/line :warn "Multiple line\nwarning sample\nhere")
+(status/line :error "Multiple line\nerror sample\nhere")
+
+(status/line :head "Any %s can use %s args" "message" "replacement")
+
+(status/line :detail "Some plain text detail")
+
+(status/die 1 "Something bad happened")
